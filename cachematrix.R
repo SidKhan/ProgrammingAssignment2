9861f4eea38a8ece008631ced0f8e93fb2dccdf1
SidKhan <nskhan1201@hotmail.com>
2017-02-02 16:00:21 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,10 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# makeCacheMatrix creates a list containing a function to# set the value of the matrix 
+# get the value of the matrix 
+# set the value of inverse of the matrix 
+# get the value of inverse of the matrix 
 makeCacheMatrix <- function(x = matrix()) {
        inv <- NULL 
        set <- function(y)     
@@ -14,16 +17,16 @@ makeCacheMatrix <- function(x = matrix()) {
         setinverse <- function(inverse)
         inv <<- inverse getinverse <- function() inv 
         list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) 
-
 }
-
-
 ## Write a short comment describing this function
-
+# The following function returns the inverse of the matrix. It first checks if 
+# the inverse has already been computed. If so, it gets the result and skips the 
+# computation. If not, it computes the inverse, sets the value in the cache via 
+# setinverse function.    
+# This function assumes that the matrix is always invertible.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        
-        inv <- x$getinverse() 
+       inv <- x$getinverse() 
         if(!is.null(inv)) 
         {  message("getting cached data.") 
          return(inv) 
@@ -34,3 +37,20 @@ cacheSolve <- function(x, ...) {
         inv 
      
 }
+               ##sample run: 
+               ## x <- matrix(data= c(4,5,6,7), nrow=2, ncol=2)
+               ## m <-makeCacheMatrix(x)
+               ## m$get()
+               ##      [,1] [,2]
+               ##[1,]    4    6
+               ##[2,]    5    7
+               ## cacheSolve(m)
+               ##      [,1] [,2]
+               ##[1,] -3.5    3
+               ##[2,]  2.5   -2 
+               ## Retrieving from the cache in the second run
+               ##cacheSolve(m)
+               ##getting cached data.
+               ##     [,1] [,2]
+               ##[1,] -3.5    3
+               ##[2,]  2.5   -2
